ba93167e43c04436dca6d5bb1ce1e74b84391387
Kgethi <97187036+kgethegodev@users.noreply.github.com>
2023-07-08 00:42:10 +0200

    [feat] added in a new color and a body font size

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,14 @@ import Sidebar from "./components/Sidebar.vue";
   --secondary: #8798AD;
   --grey: #F4F6FC;
   --dark: #2E384D;
+  --light: #BFC5D2;
   --blue: #2E5BFF;
   --white: #FFFFFF;
   --active-menu-item: #2E5BFF1A;
   --active-menu-item-border: #2E5BFF;
   --sidebar-width: 80px;
   --sidebar-width-expanded: 215px;
+  --body-font-size: 15px;
 }
 
 * {
@@ -32,6 +34,7 @@ import Sidebar from "./components/Sidebar.vue";
 
 body {
   background: var(--grey);
+  font-size: var(--body-font-size);
 }
 
 button {
