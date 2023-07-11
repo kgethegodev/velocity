b0b20767b53771dab8660ec772b58097b8722a6e
Kgethi <97187036+kgethegodev@users.noreply.github.com>
2023-07-11 05:01:40 +0200

    [feat] made menu and menu button components for usability

---
+++ src/components/MenuButton.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from "vue";
+
+const is_expanded = ref(localStorage.getItem("is_expanded") === "true");
+
+const ToggleMenu = () => {
+  is_expanded.value = !is_expanded.value;
+  localStorage.setItem("is_expanded", is_expanded.value);
+}
+</script>
+
+<template>
+  <button class="menu-toggle" @click="ToggleMenu">
+    <img src="../assets/images/menu-icons/union.svg" alt="Union Icon">
+  </button>
+</template>
+
+<style scoped lang="scss">
+
+</style>
